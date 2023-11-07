80d64f964e215c5e0180fd9f07c261a03da97c0f
guen <heloo@guen.dev>
2023-11-07 21:29:33 +0700

    init

--- packages/variants/vite.config.ts
+++ packages/variants/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: '@heartfly/composables',
+      name: 'vue-variants',
       fileName: format => `index.${format}.js`
     },
     rollupOptions: {
